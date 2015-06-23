0b39945cb5e2edcace3f736f02290f72df94fe71
vendethiel <vendethiel@hotmail.fr>
2015-06-23 17:32:41 +0200

    first v.

---
+++ src/main/scala/com/etna/easymob/MainActivity.scala
@@ -0,0 +1,13 @@
+package com.etna.easymob
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity extends Activity with TypedFindView {
+  /** Called when the activity is first created. */
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main)
+
+  }
+}

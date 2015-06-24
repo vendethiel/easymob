abe4678e4f9e5090047997a8b06f02008791a39c
vendethiel <vendethiel@hotmail.fr>
2015-06-24 18:39:55 +0200

    from the darkest night

--- build.sbt
+++ build.sbt
@@ -4,14 +4,10 @@ android.Plugin.androidBuild
 
 localProjects in Android := Nil
 
-minSdkVersion in Android := "android-21"
+minSdkVersion in Android := "android-18"
 
 targetSdkVersion in Android := "android-21"
 
 platformTarget in Android := "android-21"
 
 localAars in Android += baseDirectory.value / "libs" / "estimote-sdk-preview.aar"
-
-libraryDependencies ++= Seq(
-  "com.android.support" % "appcompat-v7" % "22.0.0"
-)
--- src/main/scala/com/etna/easymob/MainActivity.scala
+++ src/main/scala/com/etna/easymob/MainActivity.scala
@@ -1,14 +1,34 @@
 package com.etna.easymob
 
+import java.{util => ju}
+
 import android.app.Activity
-import android.os.Bundle
-//import com.es
+import android.os.{StrictMode, Bundle}
+import com.estimote.sdk.BeaconManager.RangingListener
+import com.estimote.sdk.{Region, Beacon, BeaconManager}
 
 class MainActivity extends Activity with TypedFindView {
-  /** Called when the activity is first created. */
+  val beaconMgr = new BeaconManager(this)
+  var tag : String = ""
+
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
 
+    hackThreadPolicy()
+    beaconMgr.setRangingListener(new RangingListener {
+      override def onBeaconsDiscovered(region: Region, beacons: ju.List[Beacon]): Unit = {
+        println(if (beacons.isEmpty) "hey" else ":(")
+      }
+    })
+  }
+
+  override def onStop(): Unit = {
+    finish()
+  }
+
+  private def hackThreadPolicy(): Unit = {
+    val policy = new StrictMode.ThreadPolicy.Builder().permitAll().build()
+    StrictMode.setThreadPolicy(policy)
   }
 }
